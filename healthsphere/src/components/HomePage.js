2cd560781d8fd8837683007bff85b1d91d21fe06
laxmikumari7 <laxmikumari25333@gmail.com>
2024-12-01 20:16:29 +0530

    Add removed files to .gitignore

---
+++ healthsphere/src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to HealthSphere</h1>
+      <p>Your health and wellness tracker.</p>
+    </div>
+  );
+};
+
+export default HomePage;
